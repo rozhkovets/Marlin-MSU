2e5bd82398ce184becc519e1c9ccca93fa406dbe
rozhkovets <38763175+rozhkovets@users.noreply.github.com>
2024-10-06 09:35:03 +0500

    Marlin-bugfix-2.1.x-8c61606(29.09.24)

---
+++ Marlin/Marlin.ino.18.cpp
@@ -0,0 +1,3 @@
+# 1 "C:\\Users\\A4F7~1\\AppData\\Local\\Temp\\tmp_98wny6a"
+#include <Arduino.h>
+# 1 "D:/Андрей/hobby/3d_printer/Принтер/MSU/marlin-msu/000-006-Marlin-bugfix-2.1.x-8c61606(29.09.24)/Marlin/Marlin.ino"
