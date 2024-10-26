b85c0590baba7139e6afcd26d17d3887e794b68f
rozhkovets <38763175+rozhkovets@users.noreply.github.com>
2024-10-26 14:57:57 +0500

    trash clearing

--- Marlin/Marlin.ino.19.cpp
+++
@@ -1,3 +0,0 @@
-# 1 "C:\\Users\\A4F7~1\\AppData\\Local\\Temp\\tmpykbzg1dz"
-#include <Arduino.h>
-# 1 "D:/Андрей/hobby/3d_printer/Принтер/MSU/marlin-msu/000-006-Marlin-bugfix-2.1.x-8c61606(29.09.24)/Marlin/Marlin.ino"
